fa79b48e487910f401d697ec962c4ab558ec30e8
aydee <aydee.qm@gmail.com>
2017-09-28 20:46:30 -0500

    definiendo estilos

--- app.jsx
+++ app.jsx
@@ -1,21 +1,25 @@
 
-const Header = () => {
+const Header = (props) => {
   return (
-    <header className="header">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-8">
-            <h1>PLAYERS:{}</h1>
-            <h1>TOTAL POINTS:{}</h1>
-          </div>
-          <div className="col-md-4">
-            <div><h1>STOPWATH</h1></div>
-            <div>{}</div>
-            <div>
-              <button>START</button>
-              <button>RESET</button>
-            </div>
-          </div>
+    <header>
+      <div className="header">
+        <div className="col-md-10">
+          <table className="stats">
+            <tr>
+              <td>PLAYERS:</td>
+              <td>{props.players.length}</td>
+            </tr>
+            <tr>
+              <td>TOTAL POINTS:</td>
+              <td>{}</td>
+            </tr>
+          </table>
+        </div>
+        <div className="stopwatch">
+          <h2>STOPWATH</h2>
+          <div className="stopwatch-time">{}</div>
+          <button>start</button>
+          <button>reset</button>
         </div>
       </div>
     </header>
@@ -51,15 +55,17 @@ const PlayerList = () => {
 
 const PlayerForm = () => {
   return (
-    <div className="container">
-        <input type="text" placeholder="ENTER A NAME"/>
-        <button>ADD PLAYER</button>
+    <div className="add-player-form">
+      <form action="">
+        <input type="text" placeholder="ENTER A NAME" />
+        <input type="submit" value="Add Player"/>
+      </form>
     </div>
   );
 }
 
 
-let PLAYERS = [
+let players = [
   {
     name: "Jim Hoskins",
     score: 31,
@@ -87,4 +93,4 @@ const Application = ({ title, players }) => {
   );
 }
 
-ReactDOM.render(<Application title="Scoreboard" players={PLAYERS} />, document.getElementById('container'));
+ReactDOM.render(<Application title="Scoreboard" players={players} />, document.getElementById('container'));
