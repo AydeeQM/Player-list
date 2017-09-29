78ec6fafde3e6a075e3dabfa5f830e9c0938813d
aydee <aydee.qm@gmail.com>
2017-09-28 21:32:20 -0500

    agregando lista

--- app.jsx
+++ app.jsx
@@ -1,70 +1,3 @@
-
-const Header = (props) => {
-  return (
-    <header>
-      <div className="header">
-        <div className="col-md-10">
-          <table className="stats">
-            <tr>
-              <td>PLAYERS:</td>
-              <td>{props.players.length}</td>
-            </tr>
-            <tr>
-              <td>TOTAL POINTS:</td>
-              <td>{}</td>
-            </tr>
-          </table>
-        </div>
-        <div className="stopwatch">
-          <h2>STOPWATH</h2>
-          <div className="stopwatch-time">{}</div>
-          <button>start</button>
-          <button>reset</button>
-        </div>
-      </div>
-    </header>
-  );
-}
-
-const PlayerList = () => {
-  return (
-    <div className="container">
-      <table>
-        <tr>
-          <td></td>
-          <td><button className="counter-action decrement">-</button></td>
-          <td></td>
-          <td><button className="counter-action increment">+</button></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td><button className="counter-action decrement">-</button></td>
-          <td></td>
-          <td><button className="counter-action increment">+</button></td>
-        </tr>
-        <tr>
-          <td></td>
-          <td><button className="counter-action decrement">-</button></td>
-          <td></td>
-          <td><button className="counter-action increment">+</button></td>
-        </tr>
-      </table>
-    </div>
-  );
-}
-
-const PlayerForm = () => {
-  return (
-    <div className="add-player-form">
-      <form action="">
-        <input type="text" placeholder="ENTER A NAME" />
-        <input type="submit" value="Add Player"/>
-      </form>
-    </div>
-  );
-}
-
-
 let players = [
   {
     name: "Jim Hoskins",
@@ -82,6 +15,63 @@ let players = [
     id: 3,
   },
 ];
+
+const Header = (props) => {
+  return (
+    <header>
+      <div className="header">
+        <div className="col-md-10">
+          <table className="stats"><tbody>
+            <tr>
+              <td>PLAYERS:</td>
+              <td>{props.players.length}</td>
+            </tr>
+            <tr>
+              <td>TOTAL POINTS:</td>
+              <td>{}</td>
+            </tr>
+          </tbody></table>
+        </div>
+        <div className="stopwatch">
+          <h2>STOPWATH</h2>
+          <div className="stopwatch-time">{}</div>
+          <button>start</button>
+          <button>reset</button>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+const PlayerList = ({ players }) => {
+  return (
+    <div>
+      {
+        players.map((item, index) => {
+          return (
+            <div key={index} className="player">
+              <div className="player-name col-md-10">{item.name}</div>
+              <div className="player-score counter">
+                <div className="counter-action decrement">-</div>
+                <div className="counter-score">{item.score}</div>
+                <div className="counter-action increment">+</div>
+              </div>
+            </div>);
+        })
+      }
+    </div>);
+}
+
+const PlayerForm = () => {
+  return (
+    <div className="add-player-form">
+      <form action="">
+        <input type="text" placeholder="enter name" />
+        <input type="submit" value="Add Player" />
+      </form>
+    </div>
+  );
+}
 
 const Application = ({ title, players }) => {
   return (
